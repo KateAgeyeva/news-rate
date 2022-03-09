d4c47bcf7a6a73d60d5a522b623fb535774db7e2
Kate Ageyeva <kate.ag.au@gmail.com>
2022-03-09 17:59:55 +0100

    jest tests

---
+++ __tests__/NewsList.test.jsx
@@ -0,0 +1,14 @@
+import { render, screen } from '@testing-library/react';
+import NewsList from '../components/news/NewsList';
+
+describe('NewsList', () => {
+    it('renders news if request succeeds', async() => {
+        window.fetch = jest.fn();
+        window.fetch.mockResolvedValueOnce({
+            json: async () => [{ id: 'Title', url: 'www.url.com', date: new Date() }],
+        });
+        render(<NewsList />)
+        const list = await screen.findAllByRole('listitem')
+        expect(list).not.toHaveLength(0);
+    });
+})
--- components/news/NewsItem.js
+++ components/news/NewsItem.js
@@ -2,11 +2,11 @@
 const NewsItem = ({ id, url, date }) => {
     const published = date.slice(0,10);
   return (
-        <div className="flex flex-col grid justify-items-center bg-sky-50 rounded shadow-md p-8">
+        <li className="flex flex-col grid justify-items-center bg-sky-50 rounded shadow-md p-8">
               <p className="text-lg text-center">{id}</p>
-              <p>Pablished At: {published}</p>
+              <p>Published At: {published}</p>
               <a href={url} target="_blank">Read more</a>
-        </div>
+        </li>
   );
 };
 
