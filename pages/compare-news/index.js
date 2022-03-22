3313a44382e3ab7edb231e694bcdbdd81fea712e
Kate Ageyeva <kate.ag.au@gmail.com>
2022-03-22 16:26:28 +0100

    Add Search btn align

--- pages/compare-news/index.js
+++ pages/compare-news/index.js
@@ -90,34 +90,32 @@ const CompareNews = (props) => {
               onEndDate={(date) => setEndDate(date)}
               format="yyy-MM-dd"
             />
-            <div className="flex justify-center">
-              <div className="flex flex-col">
-                {inputList.map((item, i) => (
-                  <InputField key={i} results={getResults} text={item} />
-                ))}
-
-                {inputList.length <= 5 && (
-                  <button
-                    onClick={onAddInputField}
-                    className="bg-white text-black py-2 text-white rounded w-21 mb-5"
-                  >
-                    <div className="flex flex-row justify-center items-center">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="h-6 w-6"
-                        viewBox="0 0 20 20"
-                        fill="#0EA5E9"
-                        >
-                        <path
-                          fillRule="evenodd"
-                          d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-11a1 1 0 10-2 0v2H7a1 1 0 100 2h2v2a1 1 0 102 0v-2h2a1 1 0 100-2h-2V7z"
-                          clipRule="evenodd"
-                          />
-                      </svg>
-                      <p className='ml-1'>Add Search</p>
-                    </div>
-                  </button>
-                )}
+              <div className="flex flex-col items-center">
+                  {inputList.map((item, i) => (
+                    <InputField key={i} results={getResults} text={item} />
+                  ))}
+                  {inputList.length <= 5 && (
+                    <button
+                      onClick={onAddInputField}
+                      className="text-black py-2 text-white rounded w-25 mb-5"
+                    >
+                      <div className="flex flex-row justify-center items-center">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          className="h-6 w-6"
+                          viewBox="0 0 20 20"
+                          fill="#0EA5E9"
+                          >
+                          <path
+                            fillRule="evenodd"
+                            d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-11a1 1 0 10-2 0v2H7a1 1 0 100 2h2v2a1 1 0 102 0v-2h2a1 1 0 100-2h-2V7z"
+                            clipRule="evenodd"
+                            />
+                        </svg>
+                        <p className='ml-1'>Add Search</p>
+                      </div>
+                    </button>
+                  )}
 
                 <div className="flex justify-center">
                   <button
@@ -127,7 +125,6 @@ const CompareNews = (props) => {
                     Submit
                   </button>
                 </div>
-              </div>
             </div>
           </form>
         </div>
